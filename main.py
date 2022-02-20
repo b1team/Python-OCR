4c6980586f585ce4f99b6ff41060405a7e6b178d
tungnguyentu <tungnguyentu@bizflycloud.vn>
2022-02-20 12:10:47 +0700

    feat generate qrcode

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from backend.setting import Config
 from backend.routers.home import home_bp
 from backend.routers.scan import scan_bp
 from backend.routers.result import result_bp
+from backend.routers.qr_image import qr_bp
 
 app.register_blueprint(home_bp)
 app.register_blueprint(scan_bp)
 app.register_blueprint(result_bp)
-
+app.register_blueprint(qr_bp)
 
 if __name__ == "__main__":
     pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract"
