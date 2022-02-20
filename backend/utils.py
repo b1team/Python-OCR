429e211447519fa954ea118a2e40923b2fc346af
tungnguyentu <tungnguyentu@bizflycloud.vn>
2022-02-20 12:36:46 +0700

    add download file from url

--- backend/utils.py
+++ backend/utils.py
@@ -2,6 +2,7 @@ import io
 
 import pytesseract
 from PIL import Image
+import requests
 
 
 def detect_file(file, lang):
@@ -9,3 +10,13 @@ def detect_file(file, lang):
         Image.open(io.BytesIO(file)), lang=lang
     )
     return scanned_text
+
+
+def download_file_from_url(url, file_name):
+    r = requests.get(url, stream=True)
+    with open(file_name, "wb") as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+                f.flush()
+    return file_name
